4a60edb5dcd24bac033f48ee824fb1fec37545eb
Rahul Yedida <rahul.yedida@pm.me>
2023-04-15 11:16:05 -0400

    Code cleanup

--- defect-prediction/rm-jobs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Kills all jobs and deletes outputs.
-for i in $(eval echo {$1..$2})
-do
-	scancel $i
-  rm slurm-$i.out
-done
--- defect-prediction/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#SBATCH -p max
-#srun -p max
-python3 -u steps.py
